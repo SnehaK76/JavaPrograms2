8b87a9287afc4e48fd8eb7f5730b68aeed79fed1
SnehaK76 <snehakhot7699@gmail.com>
2023-10-26 16:32:53 +0530

    "A commit"

---
+++ src/com/organization/abstarction/A.java
@@ -0,0 +1,15 @@
+package com.organization.abstarction;
+
+public abstract class A {
+	public void printHello() {
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+	}
+	
+	public abstract void printHi();
+	
+	public abstract void addition(int num1, int num2);
+}
